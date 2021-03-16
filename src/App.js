65db0310e5717746bfeec66eb672d36d4df4731e
unknown <claraqn@naver.com>
2021-03-17 01:22:05 +0900

    02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
+
 
 class App extends Component {
   render() {
     return (
-      <TodoListTemplate>
-        템플릿 완성
+      <TodoListTemplate form={<Form/>}>
+        <TodoItemList/>
       </TodoListTemplate>
     );
   }
