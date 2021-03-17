5905f6618ad630a5f6f13b11e487b788b4f61de8
unknown <claraqn@naver.com>
2021-03-17 18:09:47 +0900

    0317

--- src/App.js
+++ src/App.js
@@ -5,9 +5,57 @@ import TodoItemList from './components/TodoItemList';
 
 
 class App extends Component {
+  id = 3 //이미 0,1,2 가 존재하므로 3으로 설정
+  state = {
+    input: '',
+    todos: [
+      {id: 0, text: '리액트 소개', checked: false},
+      {id: 1, text: '리액트 소개', checked: true},
+      {id: 2, text: '리액트 소개', checked: false}
+    ]
+  }
+
+  handleChange = (e) =>{
+    this.setState({
+      input: e.target.value //input의 다음 바뀔 값
+    });
+  }
+
+  handleCreate = () =>{
+    const {input,todos}=this.state;
+    this.setState({
+      input: '', //인풋 비우고
+      //concat 사용해 배열에 추가->push 사용은 안됨!
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    });
+  }
+
+  handleKeyPress = (e) =>{
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+  }
+
   render() {
+    const { input } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
     return (
-      <TodoListTemplate form={<Form/>}>
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
         <TodoItemList/>
       </TodoListTemplate>
     );
